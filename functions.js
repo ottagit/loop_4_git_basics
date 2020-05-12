5f05f084bf8a0f9fe8de61d547a21f821c4e9c0c
ottagit <ottacc66@gmail.com>
2020-05-12 15:47:51 +0300

    define javascript arrays

---
+++ functions.js
@@ -0,0 +1,24 @@
+// Arrays: Collection of objects (numbers, strings, booleans, JS Objects, Maps, Sets...).
+// The collection of objects in an array are referred to as elements
+
+let arr = [1,2, true, {a: 1, b: 3}, "String"];
+
+const loop_4 = [
+  "Claire Akinyi",
+  "John Oduor",
+  "Boniface Mwangi",
+  "Mike Opondo",
+];
+
+const loop_4a = [
+  {
+    codename: "Loop 4.0",
+    intake: "April/May",
+    year: 2020,
+    // Demo pending
+    student_name: loop_4
+  }
+]
+
+let l1 = arr.length; // Bind length of arr to l1
+console.log(l1); // Log out l1
